43ce04e5cad46e24899d8c8bb3948515a0c67b58
PandjiAprillian <pandjiaprilian@outlook.com>
2021-07-11 09:12:05 +0700

    Menambahkan animasi AOS dan GASP

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,10 @@
+//* AOS Initialize
+AOS.init({
+    once: true,
+});
+
 const nav = document.getElementById('nav');
+const navLinks = nav.querySelectorAll('.nav-link');
 
 window.addEventListener('scroll', function () {
     let scrollPosition = window.scrollY;
@@ -8,6 +14,60 @@ window.addEventListener('scroll', function () {
     } else if (scrollPosition <= 60) {
         nav.classList.remove('nav-dark');
     }
+});
+
+//* GSAP Animation
+gsap.registerPlugin(TextPlugin);
+
+gsap.to('.introduce-text .hallo-h3', {
+    duration: 2, 
+    delay: 1.5,
+    text: 'Hallo,'
+});
+
+gsap.to('.introduce-text .pandjiaprillian', {
+    duration: 2, 
+    delay: 1.5,
+    text: 'I Am Pandji Aprillian'
+});
+
+gsap.to('.introduce-text .junior-wd', {
+    duration: 2, 
+    delay: 1.5,
+    text: 'Junior Web Developer'
+});
+
+gsap.from('.navbar', {
+    duration: 1.5,
+    y: '-100%',
+    opacity: 0
+});
+
+gsap.from('#hero .picture-pic', {
+    duration: 1,
+    y: -100,
+    opacity: 0
+});
+
+gsap.from('#hero .disquss', {
+    duration: 1,
+    delay: 3,
+    x: -50,
+    opacity: 0
+});
+
+gsap.from('.introduce-text', {
+    duration: 1,
+    x: -50,
+    opacity: 0,
+    delay: 0.5,
+    ease: 'back'
+});
+
+gsap.from('#hero .social-groups', {
+    duration: 1,
+    y: 100,
+    opacity: 0
 });
 
 
